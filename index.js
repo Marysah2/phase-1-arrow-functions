50edae6bbca940b7b42a987adc015dd0d29d2550
Marysah <Marysah2@gmail.com>
2025-09-17 14:28:51 +0300

    Implement divide function using function expression

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Declare a function using a function expression
+const divide = function(a, b) {
+  return a / b;
+};
+
+// 2. Declare an arrow function with two parameters
+const square = (x) => x * x;
+
+// 3. Declare an arrow function with one parameter
+const add = (a, b) => a + b;
